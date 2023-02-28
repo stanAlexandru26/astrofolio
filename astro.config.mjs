1a7b88d392659bedcd1d640dec6c449b35369e1d
stan.alexandru26 <stan.alexandru26@gmail.com>
2023-02-28 03:32:42 +0200

    add more boilerplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import tailwind from '@astrojs/tailwind';
-import { SITE } from '@/AppConfig';
-
+import { SITE } from './src/AppConfig';
+import image from '@astrojs/image';
+import compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   site: SITE.origin,
   base: SITE.basePathname,
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
-
   output: 'static',
   integrations: [
     tailwind({
@@ -19,12 +20,21 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    compress(),
+    sitemap(),
+    mdx(),
   ],
   vite: {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
+    },
+    optimizeDeps: {
+      exclude: ['limax'],
     },
   },
 });
--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,15 +1,15 @@
-const CONFIG = {
-  name: 'Stan Alexandru Stefan',
+import ogImage from './assets/images/ogImage.webp';
 
+const CONFIG = {
+  name: 'Stan Alexandru Stefan | Portfolio',
   origin: 'https://stefan-stan.dev',
   basePathname: '/',
   trailingSlash: false,
-
-  title: 'Stan Alexandru Stefan',
-  description: 'PacPac',
-
+  title: 'Stan Alexandru Stefan | Aspiring Front End Developer',
+  description:
+    'My portfolio website. I am a front end developer with a passion for building beautiful and functional websites.',
+  defaultImage: ogImage,
   defaultTheme: 'system',
-
   language: 'en',
   textDirection: 'ltr',
 };
